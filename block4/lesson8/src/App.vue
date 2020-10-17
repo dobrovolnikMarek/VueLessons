099468c18fa3771dce2e20547191cdf4a55a9ae4
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-17 22:17:26 +0300

    Lesson8

---
+++ block4/lesson8/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>{{carName}}</h1>
+    <app-car
+      :carName="carName"
+      :carYear="carYear"
+      @nameChanged = "carName = $event"></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './components/Car.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      carName: "Ford from parent",
+      carYear: 2018
+    }
+  },
+  components: {
+    "app-car": Car
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
