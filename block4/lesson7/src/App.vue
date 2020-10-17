1264b2c04fe44e8a41c3231bf1f0336e9e122016
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-17 19:10:43 +0300

    Lesson7

---
+++ block4/lesson7/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Parent: {{carName}}</h1>
+  <app-car 
+  :carName="carName"
+  :carYear="carYear"></app-car><!--:carName="carName" === :параметр="переменная" -->
+</template>
+
+<script>
+ import Car from "./components/Car" //Локально регистрируем компонент
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      carName: "Ford from parent",
+      carYear: 2018
+    }
+  },
+  components: {
+    "app-car": Car
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ block4/lesson7/src/components/Car.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="car">
+        <h3>Name: {{carName}} \ {{reverseName}}</h3>
+        <p>Year: {{carYear}}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, deleniti.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    //props: ["carName", "carYear"],//props: ["параметр которой bind'или в род. компонентне"]
+    // props: {
+    //     carName: String,
+    //     carYear: Number
+    // },
+    props: {
+        carName: {
+            type: String,
+            //required: true,
+            default: "Default Name"
+        },
+        carYear: Number
+    },
+    computed: {
+        reverseName: function(){
+            return this.carName.split("").reverse().join("");
+        }
+    }
+}
+</script>
+
+<style>
+    .car{
+        border: 1px solid black;
+    }
+
+    .car h3{
+        margin-bottom: 5px;
+    }
+</style>
