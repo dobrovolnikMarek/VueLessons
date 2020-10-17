b242bb7d06985c7b3f8720bb3843defdd0fa4e73
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-17 17:57:02 +0300

    Lesson5

---
+++ block4/lesson5/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-car></app-car>
+</template>
+
+<script>
+// import Car from "./components/Car" //Локально регистрируем компонент
+
+export default {
+  name: 'App',
+  // components: {
+  //   "app-car": Car
+  // }
+}
+</script>
+
+<style>
+
+</style>
---
+++ block4/lesson5/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Car from "./components/Car"
+
+const app = createApp(App)
+app.component("app-car",Car)//Глобально регистрируем компонент
+
+app.mount('#app')
