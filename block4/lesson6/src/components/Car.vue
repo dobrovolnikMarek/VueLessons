70617910fa0ffa9dc2d891c068908149473a1c60
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-17 18:37:37 +0300

    Lesson6

---
+++ block4/lesson6/src/components/Car.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="car">
+        <h3>Name: {{carName}}</h3>
+        <p>Year: {{carYear}}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, deleniti.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["carName", "carYear"],//props: ["параметр которой bind'или в род. компонентне"]
+}
+</script>
+
+<style>
+    .car{
+        border: 1px solid black;
+    }
+
+    .car h3{
+        margin-bottom: 5px;
+    }
+</style>
