ff19358a2deeaab94855dff2bd717ff7a46e5b6a
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-18 15:18:35 +0300

    Lesson10

---
+++ block4/lesson10/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>{{carName}}</h1>
+    <app-counter :counter="counter"></app-counter>
+    <app-car
+      :carName="carName"
+      :carYear="carYear"
+      :counter="counter"
+      :changeFunc="changeNameToAudi"
+      @counterUpdated = "counter = $event"
+      @nameChanged = "carName = $event"></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './components/Car.vue'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+    data() {
+      return {
+        carName: "Ford from parent",
+        carYear: 2018,
+        counter: 0
+      }
+    },
+    methods: {
+      changeNameToAudi() {
+        this.carName = "Audi"
+      }
+    },
+    components: {
+      "app-car": Car,
+      "app-counter": Counter
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
