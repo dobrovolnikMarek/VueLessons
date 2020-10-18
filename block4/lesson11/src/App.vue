7ee7d3f7e6bca01b9fe9db438a6221a7f04a695f
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-18 16:06:14 +0300

    Lesson11

---
+++ block4/lesson11/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>{{carName}}</h1>
+    <app-counter></app-counter>
+    <app-car
+      :carName="carName"
+      :carYear="carYear"
+      :changeFunc="changeNameToAudi"
+      @nameChanged = "carName = $event"></app-car>
+  </div>
+</template>
+
+<script>
+import Car from './components/Car.vue'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+    data() {
+      return {
+        carName: "Ford from parent",
+        carYear: 2018
+      }
+    },
+    methods: {
+      changeNameToAudi() {
+        this.carName = "Audi"
+      }
+    },
+    components: {
+      "app-car": Car,
+      "app-counter": Counter
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ block4/lesson11/src/components/Car.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="car">
+        <h3>Name: {{carName}} \ {{reverseName}}</h3>
+        <p>Year: {{carYear}}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, deleniti.</p>
+        <button @click="changeName">Change name</button>
+        <button  @click="changeFunc()">Change from parent</button>
+        <button  @click="updateCounter">Update Counter</button>
+    </div>
+</template>
+
+<script>
+import {eventEmitter} from "../main"
+export default {
+    //props: ["carName", "carYear"],//props: ["параметр которой bind'или в род. компонентне"]
+    props: {
+        carName: String,
+        carYear: Number,
+        changeFunc: Function,
+    },
+    // props: {
+    //     carName: {
+    //         type: String,
+    //         //required: true,
+    //         // default: "Default Name"
+    //     },
+    //     carYear: Number
+    // },
+    methods: {
+         changeName(){
+            this.carName = "Mazda";
+            this.$emit("nameChanged", this.carName);//1 параметр - событие которое будет прослушиваться; 
+                                      //2 параметр - значения которыми хотим уведомить другие компоненты
+        },
+        updateCounter(){
+            //eventEmitter.$emit("counterUpdated");
+            eventEmitter.$emit("counterUpdated", 3);
+        }
+    },
+    computed: {
+        reverseName: function(){
+            return this.carName.split("").reverse().join("");
+        },
+
+    }
+}
+</script>
+
+<style>
+    .car{
+        border: 1px solid black;
+    }
+
+    .car h3{
+        margin-bottom: 5px;
+    }
+</style>
