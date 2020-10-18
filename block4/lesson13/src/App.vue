70a0ce7a032fe18a59393ab7643f43aed2c21279
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-18 18:32:13 +0300

    Lesson13

---
+++ block4/lesson13/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <app-car>
+      <h2 slot="title">{{carName}} + html</h2>
+      <p slot="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, sequi.</p>
+    </app-car>
+  </div>
+</template>
+
+<script>
+import Car from './components/Car.vue'
+
+
+export default {
+    data(){
+      return{
+        carName: "Ford"
+      }
+    },
+    components: {
+      "app-car": Car
+    }
+  }
+</script>
+
+<style scoped>
+    h2{
+      color: red;
+    }
+</style>
---
+++ block4/lesson13/src/components/Car.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <slot name="title"></slot>
+        <hr>
+        <hr>
+        <slot name="text"></slot>    
+    </div>
+</template>
+
+<script>
+
+export default {
+    props:{
+        carName:{
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        padding: 10px;
+        border: 1px solid black;
+    }
+    p{
+        font-size: 20px;
+    }
+</style>
