05f59a90968990df00a34708f50eb7c14e13195f
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-19 14:48:04 +0300

    Lesson2

---
+++ block5/lesson2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+      <h2 v-colored>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: "Hello Vue"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
