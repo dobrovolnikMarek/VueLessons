270be9eec57948dadcbe124d206eb5891533fdfa
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-19 15:51:12 +0300

    Lesson3

---
+++ block5/lesson3/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <h2 v-colored v-if="visible">{{title}}</h2>
+      <button @click="visible = !visible">Toggle</button>
+      <button @click="title = 'New title'">Change title</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: "Hello Vue",
+      visible: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
