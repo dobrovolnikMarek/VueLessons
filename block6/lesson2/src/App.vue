af319bce0c0079958ebe6cfef9c51ab3565ae1b8
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 11:24:30 +0300

    Lesson2

---
+++ block6/lesson2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+      <h2>{{title}}</h2>
+      <input type="text" v-model="searchName ">
+      <ul>
+        <li v-for="name of filteredNames" :key="name">{{name}}</li>  
+      </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: "Hello Vue",
+      names: ["Max", "Tom", "Adam"],
+      searchName: ""
+    }
+  },
+  computed:{
+    filteredNames() {
+      return this.names.filter((name)=>{
+        return name.toLowerCase().indexOf(this.searchName.toLowerCase()) !== -1 
+      })
+    }
+  },
+  filters: {//Фильтры влияют только на отображение!!! Все фильтры создаются самостоятельно
+    lowercase(value){
+      return value.toLowerCase();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
