e9e39cb0b353018aa6178d2e8a8f3300ebff6682
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 14:05:12 +0300

    Lesson5

---
+++ block7/lesson5/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+     <h2>Block7</h2>
+    <select name="" id="" v-model="social">
+      <option v-for="s in socialList" :key="s">{{s}}</option>
+    </select>
+    <hr>
+    <p>{{social}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      defaultSocial: "VK",
+      social: "Instagram",
+      socialList: ["Facebook", "VK", "Instagram"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
