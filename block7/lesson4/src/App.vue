c195613ff1fb64df475a58f3bc9637865393a78b
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 13:52:00 +0300

    Lesson4

---
+++ block7/lesson4/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+     <h2>Block7</h2>
+    <label for="">
+      <input type="radio" value="inst" v-model="social">Instagram
+    </label>
+    <label for="">
+      <input type="radio" value="vk" v-model="social">VK
+    </label>
+    <label for="">
+      <input type="radio" value="fb" v-model="social">Facebook
+    </label>
+    <hr>
+    <p>{{social}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      social: "fb"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
