9e3bf91cc6328058fe7098c52fa84d4a83ace5e5
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 13:37:33 +0300

    Lesson3

---
+++ block7/lesson3/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h2>Block7</h2>
+    <label for="">
+      <input type="checkbox" value="inst" v-model="social">Instagram
+    </label>
+    <label for="">
+      <input type="checkbox" value="vk" v-model="social">VK
+    </label>
+    <label for="">
+      <input type="checkbox" value="fb" v-model="social">Facebook
+    </label>
+    <hr>
+    <ul>
+      <li v-for="s in social" :key="s">{{s}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      social: ["vk"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
