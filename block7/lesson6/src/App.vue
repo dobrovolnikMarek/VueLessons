5798f5b8d32c4c68c90476d8a993e2144fde15e8
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 14:42:02 +0300

    Lesson6

---
+++ block7/lesson6/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+     <h2>Block7</h2>
+     <input type="text" v-model.number="age">
+     <p>{{age}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      age: 20
+    }
+  },
+  watch: {
+    age(val){
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
