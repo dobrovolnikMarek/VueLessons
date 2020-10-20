9432e388fa4ad2d849b198721cad0790510741fc
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 12:22:39 +0300

    Lesson1

---
+++ block7/lesson1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h2>Block7</h2>
+    <input type="text" v-model="name">
+    <p>{{name}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      name:""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
