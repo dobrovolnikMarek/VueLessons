e0d7a8ca1ac7aca3b5982fc099c6519a5aa881f6
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 10:33:54 +0300

    Lesson1

---
+++ block6/lesson1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      <h2>{{title}}</h2>
+      <h2>{{title | lowercase}}</h2><!--вызывается фильтр через | и метод-->
+      <h2>{{title | uppercase}}</h2><!--изменяется только отображение на странице-->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      title: "Hello Vue"
+    }
+  },
+  filters: {//Фильтры влияют только на отображение!!! Все фильтры создаются самостоятельно
+    lowercase(value){
+      return value.toLowerCase();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
