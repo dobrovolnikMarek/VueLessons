aba64243913eb5e3598af49d558ce17e0a51f6cb
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-20 13:22:34 +0300

    Lesson2

---
+++ block7/lesson2/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>Block7</h2>
+    <textarea v-model="name"></textarea>
+    <p>{{name}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      name:"HELLO"
+    }
+  }
+}
+</script>
+
+<style scoped>
+  textarea{
+    height: 100px;
+    width: 400px;
+  }
+
+  p{
+    white-space: pre;
+  }
+</style>
