a8057ded6b3996f58fe31eea90c629940464de47
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-21 20:10:34 +0300

    Lesson2

---
+++ block8/lesson2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="container">
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" id="email" class="form-control" v-model="email" @blur="$v.email.$touch()">
+      </div><!--@input="$v.email.$touch()"- валидируем поле email $touch()- иницилизирует валидацию -->
+    </form> 
+
+    <pre>{{$v.email}}</pre>
+  </div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      email: ""
+    }
+  },
+  validations:{
+    email:{
+      required, 
+      email
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
