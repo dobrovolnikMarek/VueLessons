fa1a1c64080dd39deb68f47c39fd6a2e05c0cd4e
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-21 19:33:21 +0300

    Lesson1

---
+++ block8/lesson1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="container">
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" id="email" class="form-control" v-model="email">
+      </div>
+    </form> 
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      email: ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
