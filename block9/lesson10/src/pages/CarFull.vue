a1094335102cfbac0b894d67d986c726e52d69cb
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-29 15:31:17 +0200

    Lesson10

---
+++ block9/lesson10/src/pages/CarFull.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4>Car name: {{$route.query.name}}</h4>
+        <h4>Car year: {{year}}</h4>
+        <div class="scroll"></div>
+        <h6 id="scroll">hello</h6>
+        <div class="scroll"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed:{
+            year(){
+                return this.$route.query.year;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .scroll{
+        width: 100px;
+        height: 1000px;
+        background: #ccc;
+    }
+</style>
