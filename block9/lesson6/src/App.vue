f39bb91a6181d1949d1b5bd07f25c5fc2468cf39
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-10-29 10:29:42 +0200

    Lesson6

---
+++ block9/lesson6/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <router-link class="nav-item" exact tag="li" to="/" active-class="active">
+           <a class="nav-link">Home</a>
+        </router-link>
+        <router-link class="nav-item" tag="li" to="/cars" active-class="active">
+          <a class="nav-link">Cars</a>
+        </router-link>
+        <router-link class="nav-item" tag="li" to="/car/3" active-class="active">
+          <a class="nav-link">Car 3</a>
+        </router-link>
+        <router-link class="nav-item" tag="li" to="/car/4" active-class="active">
+          <a class="nav-link">Car 4</a>
+        </router-link>
+      </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+
+</style>
