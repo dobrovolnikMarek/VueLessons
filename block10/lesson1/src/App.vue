154b736e3d32d36c4c9408bb0d4dcb1c512d220f
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-11-01 17:19:49 +0200

    Lesson1

---
+++ block10/lesson1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>HELLO</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ block10/lesson1/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResourse from "vue-resource"
+
+Vue.use(VueResourse)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
