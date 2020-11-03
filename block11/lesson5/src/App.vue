51d4ea525b4da0516767ba0940cd301f44bf4002
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-11-03 17:28:56 +0200

    Lesson5

---
+++ block11/lesson5/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container text-center pt-5">
+    <app-counter></app-counter>
+    <app-second-count></app-second-count>
+    <hr>
+    <app-actions></app-actions>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+import Actions from './components/Actions'
+import SecondCounter from "./components/SecondCounter";
+
+export default {
+  components: {
+    "app-counter": Counter,
+    "app-actions": Actions,
+    "app-second-count": SecondCounter
+  }
+}
+</script>
+
+<style>
+
+</style>
