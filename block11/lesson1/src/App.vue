43c60792e9af067e0a58f44206f3db730b12f2fd
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-11-03 12:06:38 +0200

    Lesson1

---
+++ block11/lesson1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container text-center pt-5">
+    <app-counter :counter="counter"></app-counter>
+    <app-actions @counterUpdated="counter += $event"></app-actions>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter'
+import Actions from './components/Actions'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      counter: 0
+    }
+  },
+  components: {
+    "app-counter": Counter,
+    "app-actions": Actions
+  }
+}
+</script>
+
+<style>
+
+</style>
