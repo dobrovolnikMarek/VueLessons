79358b1c994430d5313038ed5135bd0b7f34da97
dobrovolnikMarek <naumchakdmytro@gmail.com>
2020-11-03 17:56:32 +0200

    Lesson6

---
+++ block11/lesson6/src/components/Actions.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button class="btn btn-success" @click="updateCounter(1)">Add</button>
+        <button class="btn-danger" @click="updateCounter(-1)">Subtract</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            updateCounter(val){
+                //this.$emit("counterUpdated", val)
+                //this.$store.state.counter += val;
+                //this.$store.commit("changeCounter", val)
+                this.$store.dispatch("asyncChangeCounter", {
+                    counterVal: val,
+                    timeoutDelay: 200
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
